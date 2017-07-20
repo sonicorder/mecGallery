5d7cc0086b30cc7c5a9d0cceca8ddc2021d7b9c7
Mike <jan.michael.jurek@gmail.com>
2017-07-19 20:59:59 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,68 @@
+// Send Request
+function request() {
+
+  let endPoint = 'https://www.mec.ca/api/v1/products/search?keywords=Mens+Road+Bikes+Ridley';
+
+  fetch(endPoint)
+    .then((response) => {
+      let myResponse = response.json();
+      return myResponse;
+    })
+    .then((productResponse) => {
+
+      // Assign response to 'bike'
+      let bike = productResponse.products;
+
+      // Hold Src
+      const imgSrc = [
+        {
+          thumb: bike[0].default_image_urls.small_image_url,
+          main:  bike[0].default_image_urls.main_image_url,
+        },
+        {
+          thumb: bike[1].default_image_urls.small_image_url,
+          main:  bike[1].default_image_urls.main_image_url,
+        },
+        {
+          thumb: bike[2].default_image_urls.small_image_url,
+          main:  bike[2].default_image_urls.main_image_url,
+        },
+        {
+          thumb: bike[3].default_image_urls.small_image_url,
+          main:  bike[3].default_image_urls.main_image_url,
+        }                        
+      ]
+
+      // Get Hero Image
+      const hero = document.querySelector('.heroImg');
+
+      // Get Thumbs
+      const thumbsImg = document.querySelectorAll('.thumb');        
+
+      // Assign initial state
+      hero.src = imgSrc[0].main;
+
+      // Assign src for Thumbs
+      for(let i = 0; i < thumbsImg.length; i++) {
+
+          thumbsImg[i].src = imgSrc[i].thumb;
+
+          thumbsImg[i].onclick = function() {
+            hero.src = imgSrc[i].main;
+          }
+
+
+
+      }      
+
+      
+
+      console.log(bike);
+    })
+    .catch((e) => {
+      console.log(`Error`);
+    });  
+
+}; 
+
+const sendIt = request();
