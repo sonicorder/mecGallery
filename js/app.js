710b6e7f34e69acd5a85263b603df025f4b5216c
Mike <jan.michael.jurek@gmail.com>
2017-07-22 16:24:35 -0700

    Arrow and Thumb navigation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Send Request
-function request() {
+const sendRequest = () => {
 
   let endPoint = 'https://www.mec.ca/api/v1/products/search?keywords=Mens+Road+Bikes+Ridley';
 
@@ -18,15 +18,43 @@ function request() {
 
       // Get Hero Image
       const hero = document.querySelectorAll('.hero-img.container img');
+      
+      // Assign initial "State" for Hero Img.
+      let state = 0;  
+
+      // Loop through and remove 'current' class
+      const removeCurrentClass = () => {
+        for(let k = 0; k < hero.length; k++) {
+          hero[k].className = "";
+        }
+      }
+
+      // Nav Rewind
+      const rw = () => {
+        if(state > 0) {
+          state -= 1;    
+          removeCurrentClass();
+          hero[state].className = "current";              
+        }              
+      }
+
+      // Nav FF
+      const ff = () => {
+        if(state < 3) {
+          state += 1;
+          removeCurrentClass();
+          hero[state].className = "current";              
+        }
+      } 
 
       function clickity() {
 
         if( hero[ Number(this.className) - 1 ].className.indexOf('current') > -1 ) {
           // console.log("Already selected");
         }else {
-
-          for(var j = 0; j < hero.length; j++) {
-            hero[j].className = "";
+            state = Number(this.className) - 1;
+          for(let j = 0; j < hero.length; j++) {
+            removeCurrentClass();
             hero[ Number(this.className) - 1 ].className = "current";
           } // End For
         
@@ -34,27 +62,34 @@ function request() {
         
       }
 
+      // Get Nav Arrows Assign RW/FF
+      const before = document.querySelector('.before')
+                     .onclick = rw;
+      const after = document.querySelector('.after')
+                    .onclick = ff;         
+
       // Get Thumbs
       const thumbsImg = document.querySelectorAll('.thumbs a img');       
 
       // Loop Through Thumbs
       for(let i = 0; i < thumbsImg.length; i++) {
           
-          // Populate imgInfo
+          // Populate imgInfo object
           imgInfo[i] = {
             thumb: bike[i].default_image_urls.small_image_url,
             main:  bike[i].default_image_urls.main_image_url,
             name:  bike[i].full_name            
           }
 
-          // Assign src for Thumbs
+          // Assign src to Thumbs
           thumbsImg[i].src = imgInfo[i].thumb;
           thumbsImg[i].alt = imgInfo[i].name;
 
-          // Assign initial state
+          // Assign initial Hero position
           hero[i].src = imgInfo[i].main;
           hero[i].alt = imgInfo[i].name;          
 
+          
           thumbsImg[i].onclick = clickity;      
 
       } // End outer for      
@@ -66,4 +101,4 @@ function request() {
 
 }; 
 
-const sendIt = request();
+sendRequest();
