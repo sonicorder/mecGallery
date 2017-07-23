adfacefff549252a65cda59c6a3047835803c8aa
Mike <jan.michael.jurek@gmail.com>
2017-07-22 22:16:06 -0700

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
 const gulp = require('gulp');  
 const sass = require('gulp-sass');
 const uglifycss = require('gulp-uglifycss');
+const babel = require('gulp-babel');
+var rename = require('gulp-rename');  
+var uglify = require('gulp-uglify'); 
+    
+//script paths
+var jsFiles = 'assets/scripts/**/*.js',  
+    jsDest = 'dist/scripts';
+
+gulp.task('scripts', function() {  
+    return gulp.src(jsFiles)
+        .pipe(rename('scripts.min.js'))
+        .pipe(babel({
+            presets: ['es2015']
+        }))        
+        .pipe(uglify())
+        .pipe(gulp.dest(jsDest));
+});
 
 //style paths
 const sassFiles = 'styles/sass/*.scss',  
@@ -20,4 +37,5 @@ gulp.task('styles', function(){
 //WATCH FOR CHANGES
 gulp.task('watch',function() {  
     gulp.watch(sassFiles,['styles']);
+    gulp.watch(jsFiles, ['scripts']);
 });
